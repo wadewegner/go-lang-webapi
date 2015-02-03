3927ce56fb685a26ddb8e979b9c593a9c63add97
Wade Wegner <wade.wegner@gmail.com>
2015-02-03 08:55:01 -0700

    Updated to use proper OS port

--- src/app.go
+++ src/app.go
@@ -4,10 +4,18 @@ import (
     "github.com/emicklei/go-restful"
     "log"
     "net/http"
+    "os"
     "./userservice"
 )
  
 func main() {
+
+	httpPort := os.Getenv("HTTP_PLATFORM_PORT")
+
+	if (len(httpPort) == 0) {
+		httpPort = "8080"
+	}
+
     restful.Add(userservice.New())
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":" + httpPort, nil))
 }
